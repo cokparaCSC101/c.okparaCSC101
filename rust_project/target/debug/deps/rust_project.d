a20841401992c904ea47d85a834cfe43ec20e4b4
cokparaCSC101 <clintonokpara22@gmail.com>
2022-10-30 22:02:22 +0100

    My first commit

---
+++ rust_project/target/debug/deps/rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\Olivia Juice\OneDrive\Documents\c.okparaCSC101\rust_project\target\debug\deps\rust_project.exe: src\main.rs
+
+C:\Users\Olivia Juice\OneDrive\Documents\c.okparaCSC101\rust_project\target\debug\deps\rust_project.d: src\main.rs
+
+src\main.rs:
---
+++ rust_project/target/debug/rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\Olivia\ Juice\OneDrive\Documents\c.okparaCSC101\rust_project\target\debug\rust_project.exe: C:\Users\Olivia\ Juice\OneDrive\Documents\c.okparaCSC101\rust_project\src\main.rs
